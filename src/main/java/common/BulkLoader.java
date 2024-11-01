29307a9cfc4192ed93b24d2d7e1abd4b163bcf3b
Nicholas Varela <nicholasjv@icloud.com>
2024-10-31 20:22:49 -0400

    Added scanAndSort skeleton

--- src/main/java/common/BulkLoader.java
+++ src/main/java/common/BulkLoader.java
@@ -1,7 +1,9 @@
 package common;
 import java.io.*;
 import java.util.*;
-import java.util.Scanner;
+
+import file_management.TupleReader;
+import java.util.ArrayList;
 
 
 public class BulkLoader {
@@ -9,9 +11,11 @@ public class BulkLoader {
     private int d; // order of the tree 
     private String relationName; 
     private boolean isClustered; 
+    private List<DataEntry> dataEntries;
     
     public BulkLoader(String indexInfoFilePath) throws IOException {
         parseIndexInfoFile(indexInfoFilePath);
+        dataEntries = new ArrayList<>();
     }
     private void parseIndexInfoFile(String indexInfoFilePath) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(indexInfoFilePath))) {
@@ -27,7 +31,35 @@ public class BulkLoader {
         }
     }
 
-    
+    public void scanAndSortRelation(String relationPath){
+        try(TupleReader reader = new TupleReader(relationPath)){
+           List<int[]> tuples = reader.readTuples();
+            for (int[] tuple : tuples) {
+                // Assuming the first element is the key
+                int key = tuple[0];
+                DataEntry entry = new DataEntry(key, tuple);
+                dataEntries.add(entry);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            
+    }
 
+    public class DataEntry implements Comparable<DataEntry>{
+        int key;
+        int[] tuple;
+    
+        public DataEntry(int key, int[] tuple) {
+            this.key = key;
+            this.tuple = tuple;
+        }
+    
+        @Override
+        public int compareTo(DataEntry other) {
+            return Integer.compare(this.key, other.key);
+        }
+    }
 
 }
+
